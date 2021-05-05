ff259d4c676474038673a746367b137e9035fe6b
SimonBr017 <simonbrard017@gmail.com>
2021-05-05 11:04:07 +0200

    fix pep8 t6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for ligne in matrix:
-        for column in ligne:
-            print("{:d}".format(column), end=" " if column != ligne[-1] else "")
+        for coln in ligne:
+            print("{:d}".format(coln), end=" " if coln != ligne[-1] else "")
         print()
