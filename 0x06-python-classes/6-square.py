0474ca4b0a52172c7f7f2d0b2e10fc32a8dff31d
SimonBr017 <simonbrard017@gmail.com>
2021-05-18 20:19:00 +0200

    truc pas comprendre

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -5,7 +5,7 @@
 class Square:
     """class with atribute that define a Square"""
     def __init__(self, size=0, position=(0, 0)):
-        self.__size = size
+        self.size = size
         self.position = position
 
     @property
