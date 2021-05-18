1f78265c10059059f05f505fe7fb4bc252ab8b34
SimonBr017 <simonbrard017@gmail.com>
2021-05-18 12:55:24 +0200

    add T1 fix pep8 t0

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""class Square that defines a square"""
+
+
+class Square:
+    """class with atribute that define a Square"""
+    def __init__(self, size):
+        self.__size = size
