4db5e8c2d3ba69a692cbb01349ea265d3c15e9a7
SimonBr017 <simonbrard017@gmail.com>
2021-05-18 13:03:05 +0200

    add T2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""class Square that defines a square"""
+
+
+class Square:
+    """class with atribute that define a Square"""
+    def __init__(self, size=0):
+        self.__size = size
+        if type(self.__size) is not int:
+            raise TypeError("size must be an integer")
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
