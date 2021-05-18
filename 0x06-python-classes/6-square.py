37df210b112d3ccfda59bf7f86ec21a1cc36382c
SimonBr017 <simonbrard017@gmail.com>
2021-05-18 20:22:01 +0200

    fix pep8?

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -5,7 +5,7 @@
 class Square:
     """class with atribute that define a Square"""
     def __init__(self, size=0, position=(0, 0)):
-        self.size = size
+        self.__size = size
         self.position = position
 
     @property
@@ -27,9 +27,11 @@ class Square:
     @position.setter
     def position(self, value):
         self.__position = value
-        if (type(value) is not tuple or len(value) != 2
-                or type(value[0]) is not int or type(value[1]) is not int
-                or value[0] < 0 or value[1] < 0):
+        if type(value) is not tuple or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if type(value[0]) is not int or type(value[1]) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
 
     def area(self):
