983819ff9053502bbaea011da995beaa97334134
SimonBr017 <simonbrard017@gmail.com>
2021-05-20 19:25:14 +0200

    add t5

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""unction that prints a text with 2 new lines 
+after each of these characters: ., ? and :"""
+
+
+def text_indentation(text):
+    string=""
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    i = 0
+    for c in text:
+        if i == 0:
+            if c == ' ':
+                continue
+            else:
+                i = 1
+        if i == 1:
+            string += c
+        if c in ('.', '?', ':'):
+            string += "\n\n"
+            i = 0
+    print(string)
