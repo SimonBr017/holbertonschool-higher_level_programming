c0db6b9ee66f3e6cc8267d9e870be8313adbae43
SimonBr017 <simonbrard017@gmail.com>
2021-05-22 16:40:10 +0200

    backup t5 & add comm

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -4,12 +4,24 @@ after each of these characters: ., ? and :"""
 
 
 def text_indentation(text):
+    """text must be a string, otherwise raise a TypeError
+    exception with the message text must be a string
+    There should be no space at the beginning or
+    at the end of each printed line"""
+
     string = ""
     if type(text) is not str:
         raise TypeError("text must be a string")
-
+    i = 0
     for c in text:
-        string += c
+        if i == 0:
+            if c == ' ':
+                continue
+            else:
+                i = 1
+        if i == 1:
+            string += c
         if c in ('.', '?', ':'):
             string += "\n\n"
+            i = 0
     print(string)
