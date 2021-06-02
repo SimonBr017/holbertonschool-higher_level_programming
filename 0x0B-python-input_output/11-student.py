43d56b7ef06955b29bed7b14cae86c72908d7568
SimonBr017 <simonbrard017@gmail.com>
2021-06-02 11:57:14 +0200

    add 10 & 11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -4,7 +4,6 @@
 
 class Student:
     """"define student"""
-
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
@@ -23,3 +22,6 @@ class Student:
                 dict[i] = getattr(self, i)
 
         return dict
+
+    def reload_from_json(self, json):
+        self.__dict__ = json
