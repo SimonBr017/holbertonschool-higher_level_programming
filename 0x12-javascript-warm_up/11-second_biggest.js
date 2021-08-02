293ec892d88f0b1d90e4ccd7f1a62146b787c847
SimonBr017 <simonbrard017@gmail.com>
2021-08-02 15:07:10 +0200

    add 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -8,7 +8,7 @@ if (process.argv.length < 4) {
   console.log('0');
 } else {
   const arr = process.argv.slice(2); // select elements from an array
-  intArr = arr.map(Number); // convert into number
+  const intArr = arr.map(Number); // convert into number
   const secondLargestNumber = intArr.sort((a, b) => {
     return b - a;
   })[1];
