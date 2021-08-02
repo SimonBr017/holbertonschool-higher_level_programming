9ca463e156486f12068df02bffa5ac44ebd14fbb
SimonBr017 <simonbrard017@gmail.com>
2021-08-02 12:19:18 +0200

    fix pebkak

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -5,7 +5,7 @@
  */
 
 if (process.argv[2] === undefined) {
-    console.log('No argument');
+  console.log('No argument');
 } else {
-    console.log(process.argv[2]);
+  console.log(process.argv[2]);
 }
