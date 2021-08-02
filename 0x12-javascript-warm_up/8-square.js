df89874e1992b1944f15cdd4448587b980e628f6
SimonBr017 <simonbrard017@gmail.com>
2021-08-02 13:34:25 +0200

    add t8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+/**
+ * script that prints a square
+ */
+
+const x = parseInt(process.argv[2]);
+
+if (isNaN(x)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log('X'.repeat(x));
+  }
+}
