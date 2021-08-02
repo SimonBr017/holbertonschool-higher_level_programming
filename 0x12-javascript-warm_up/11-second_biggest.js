413964dc7feee35b97718a5479f1cb4bac28bddb
SimonBr017 <simonbrard017@gmail.com>
2021-08-02 15:02:59 +0200

    add 11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+/**
+ * script that searches the second biggest integer in the list of arguments.
+ */
+if (process.argv.length < 4) {
+  console.log(0);
+} else {
+  const arr = process.argv.slice(2); // select elements from an array
+  intArr = arr.map(Number); // convert into number
+  const secondLargestNumber = intArr.sort((a, b) => {
+    return b - a;
+  })[1];
+  console.log(secondLargestNumber);
+}
