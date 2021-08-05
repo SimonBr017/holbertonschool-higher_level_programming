3fa03ff16d022dbc7ec96e9aabb4721089bb0f4a
SimonBr017 <simonbrard017@gmail.com>
2021-08-05 16:16:25 +0200

    fix t2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python3
 """script that lists all states from the database hbtn_0e_0_usa"""
 
+
 import MySQLdb
 import sys
 
+
 def main():
 
-    connection_data_base = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    connection_data_base = MySQLdb.connect(host="localhost",
+                                           port=3306,
+                                           user=sys.argv[1],
+                                           passwd=sys.argv[2],
+                                           db=sys.argv[3])
 
     cursor = connection_data_base.cursor()
 
-    cursor.execute("SELECT * FROM states WHERE BINARY name = '{}' ORDER BY id ASC".format(sys.argv[4]))
+    cursor.execute(
+        "SELECT * FROM states WHERE BINARY name = '{}'ORDER BY id ASC".format(
+            sys.argv[4]))
 
     query_rows = cursor.fetchall()
 
@@ -18,6 +26,7 @@ def main():
         print(row)
     cursor.close()
     connection_data_base.close()
+
 
 if __name__ == '__main__':
     main()
