12e103ea4a28b290475c01e8d65ed617ffddff99
SimonBr017 <simonbrard017@gmail.com>
2021-08-05 16:18:16 +0200

    fix t3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
 """script that lists all states from the database hbtn_0e_0_usa"""
 
+
 import MySQLdb
 import sys
 
+
 def main():
 
-    connection_data_base = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    connection_data_base = MySQLdb.connect(host="localhost",
+                                           port=3306,
+                                           user=sys.argv[1],
+                                           passwd=sys.argv[2],
+                                           db=sys.argv[3])
 
     cursor = connection_data_base.cursor()
 
-    cursor.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC",(sys.argv[4],))
+    cursor.execute(
+        "SELECT * FROM states WHERE name = %s ORDER BY id ASC", (sys.argv[4],))
 
     query_rows = cursor.fetchall()
 
@@ -18,6 +25,7 @@ def main():
         print(row)
     cursor.close()
     connection_data_base.close()
+
 
 if __name__ == '__main__':
     main()
