d8bda5f5647ee51478d738f95666eb9eeba0921a
SimonBr017 <simonbrard017@gmail.com>
2021-08-05 16:22:03 +0200

    fix pep8 t4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python3
 """script that lists all cities from the database hbtn_0e_4_usa"""
 
+
 import MySQLdb
 import sys
 
+
 def main():
 
-    connection_data_base = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    connection_data_base = MySQLdb.connect(host="localhost",
+                                           port=3306,
+                                           user=sys.argv[1],
+                                           passwd=sys.argv[2],
+                                           db=sys.argv[3])
 
     cursor = connection_data_base.cursor()
 
-    cursor.execute("SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON states.id = cities.state_id ORDER BY cities.id")
+    cursor.execute(
+        "SELECT cities.id, cities.name, states.name FROM \
+        INNER JOIN states ON states.id = cities.state_id ORDER BY cities.id")
 
     query_rows = cursor.fetchall()
 
@@ -18,6 +26,7 @@ def main():
         print(row)
     cursor.close()
     connection_data_base.close()
+
 
 if __name__ == '__main__':
     main()
