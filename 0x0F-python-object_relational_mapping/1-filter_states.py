ec22de7ed903677feecf3f38b727da7e231f4b14
SimonBr017 <simonbrard017@gmail.com>
2021-08-05 16:00:46 +0200

    fix pep8 t1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
 """script that lists all states from the database hbtn_0e_0_usa"""
 
+
 import MySQLdb
 import sys
 
+
 def main():
 
-    connection_data_base = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    connection_data_base = MySQLdb.connect(host="localhost",
+                                           port=3306,
+                                           user=sys.argv[1],
+                                           passwd=sys.argv[2],
+                                           db=sys.argv[3])
 
     cursor = connection_data_base.cursor()
 
@@ -18,6 +24,7 @@ def main():
         print(row)
     cursor.close()
     connection_data_base.close()
+
 
 if __name__ == '__main__':
     main()
