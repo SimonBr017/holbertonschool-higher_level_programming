ee8bab5fcd8aaf387277f0cf0f0b414dd09205f1
SimonBr017 <simonbrard017@gmail.com>
2021-09-14 12:03:42 +0200

    please checker

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -8,7 +8,6 @@ const fs = require('fs');
 
 fs.appendFile(process.argv[2], process.argv[3], function (err) {
   if (err) {
-    console.log('append failed');
     console.error(err);
   } else {
     console.log('Done');
