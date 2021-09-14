cd7912a43d37dff25fcd26e7cac97523b50ea54e
SimonBr017 <simonbrard017@gmail.com>
2021-09-14 12:06:02 +0200

    please checker

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -6,10 +6,8 @@
 
 const fs = require('fs');
 
-fs.writeFile(process.argv[2], process.argv[3], function (err) {
+fs.appendFile(process.argv[2], process.argv[3], function (err) {
   if (err) {
     console.error(err);
-  } else {
-    console.log('Done');
   }
 });
